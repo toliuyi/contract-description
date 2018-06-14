eedb21d9ca5d87706c7290666c3ade8c2a52ae61
义哥 <liuyi3@gmail.com>
2018-06-14 13:17:31 +0800

    change into to desc

---
+++ contracts/ContractDescription.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.4.20;
+
+/**
+   @title Owned, a standard interface to check smart contract owner
+
+   Any contract implemente ERC*** should support this inferface, that to say has public ower() function to 
+   return owner's address
+
+ */
+contract Owned {
+  /**
+   * @dev Return contract owner address. 
+   * @return owner address
+   */
+  function owner() public view returns (address);
+}
+
+/**
+   @title ContractInfo Human readable contract description registry. Implimentation of ERC***.
+
+   Allow contract owner attach human readable description to contract and it's functions. Wallets such as
+   MetaMask, Imtoken, Status, Toshi etc. could display those descriptions to users when they about to confirm
+   Ethereum transaction.
+
+ */
+contract ContractDescription {
+
+  //inforamtion storage, contract address => function selector => language code => description
+  mapping (address => mapping( bytes4 =>mapping (bytes5 => string))) public desc_store;
+
+  //event emitted after contract description got set
+  event DescriptionAttached(address indexed contractAddr, bytes4 selector, bytes5 lang, string desc);
+
+  /**
+   * @dev Throws if called by any account other than contract owner.
+   */
+  modifier onlyContractOwner(address contractAddr){
+    require(Owned(contractAddr).owner() == msg.sender);
+    _;
+  }
+
+  /**
+   * @dev Allows the contract owner to attach human readable description of contract and it's functions.
+   * @param contractAddr The address of the contract which info attached to.
+   * @param selector The selector of function which info attached to, use contract name's selector to attach
+   description for the whole contract.
+   * @param lang ISO 639 language code of the description. Every contract should at least attach english info
+   which language code is "en".
+   * @param description in UTF8 encoding.
+   */
+  function attachDesc(address contractAddr, bytes4 selector, bytes5 lang, string desc)
+   public onlyContractOwner(contractAddr){
+    desc_store[contractAddr][selector][lang] = desc;
+    emit DescriptionAttached(contractAddr, selector, lang, desc);
+  }
+
+  /**
+   * @dev Allows anyone query contract description from this registry.
+   * @param contractAddr The address of the contract to query.
+   * @param selector The selector of function to query, use contract name's selector to query
+   description for the whole contract.
+   * @param lang ISO 639 language code of the description. If there is no description attached in specified language, "en" used as defaut.
+   * @return description string in UTF8 encoding.
+   */
+  function getDesc(address contractAddr, bytes4 selector, bytes5 lang) constant public returns (string) {
+    if(bytes(desc_store[contractAddr][selector][lang]).length != 0) return desc_store[contractAddr][selector][lang];
+    else return desc_store[contractAddr][selector]["en"];
+  }
+
+}
